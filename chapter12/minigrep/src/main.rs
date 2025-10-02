7132c73c501b38f8cf59ce1cd1f0825f47b46732
devsula50 <devsula50@gmail.com>
2025-10-02 17:48:44 +0900

    Using the search function in the run function

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -8,10 +8,6 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    // let (query, file_path) = parse_config(&args);
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
