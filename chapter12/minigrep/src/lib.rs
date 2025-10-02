d40157b30be7e6cabc2edf5a64c0073fb443d20c
devsula50 <devsula50@gmail.com>
2025-10-02 17:44:37 +0900

    Writing code to pass the test
    1. Iterate through each line of the contents.
    2. Check whether the line contains our query string.
    3. If it does, add it to the list of values we're returning.
    4. If it doesn't, do nothing.
    5. Return the list of results that match.

--- chapter12/minigrep/src/lib.rs
+++ chapter12/minigrep/src/lib.rs
@@ -35,9 +35,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(
             vec!["safe, fast, productive."],
@@ -46,6 +46,14 @@ mod tests {
     }
 
     pub fn search<'a>(query: &str, contents: &'a str,) -> Vec<&'a str> {
-        vec![]
+        let mut results = Vec::new();
+
+        for line in contents.lines() {
+            if line.contains(query) {
+                results.push(line);
+            }
+        }
+
+        results
     }
 }
